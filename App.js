7cd9f7804eea8e1d7b431547141c33550cc044ec
Furkan Uğuz <uguzdev@gmail.com>
2022-09-08 16:53:35 +0300

    htmltopdf

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Alert} from 'react-native';
 import React from 'react';
 import RNHTMLtoPDF from 'react-native-html-to-pdf';
 import htmlPlace from './src/helpers';
 import PDFtoHTML from './src/pages/pdf-to-html';
+import FileViewer from 'react-native-file-viewer';
 
 const App = () => {
   const createPDF = async () => {
@@ -14,11 +15,31 @@ const App = () => {
       };
       let file = await RNHTMLtoPDF.convert(options);
       console.log(file.filePath);
-      alert(file.filePath);
+      Alert.alert(
+        'Successfully Exported',
+        'Path:' + file.filePath,
+        [
+          {text: 'Cancel', style: 'cancel'},
+          {text: 'Open', onPress: () => openFile(file.filePath)},
+        ],
+        {cancelable: true},
+      );
     } catch (error) {
       console.log(error);
     }
   };
+
+  const openFile = filepath => {
+    const path = filepath; // absolute-path-to-my-local-file.
+    FileViewer.open(path)
+      .then(() => {
+        // success
+      })
+      .catch(error => {
+        // error
+      });
+  };
+
   return (
     <View style={styles.container}>
       <PDFtoHTML onPress={() => createPDF()} />
